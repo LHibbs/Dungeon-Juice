ceb29d89f2f42b15e99ea41e980ddf98743ee1b9
JessClark2755 <jclark2755@gmail.com>
2019-01-26 19:51:22 -0800

    changes

--- Assets/Scripts/Darkness.cs
+++ Assets/Scripts/Darkness.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Darkness : MonoBehaviour
 {
     private GameObject player;
+    public float scaleFactor = 0.0025f;
+
+    private float multiplier = 1f;
     public int darknessHeight;
 
     // Start is called before the first frame update
@@ -17,23 +20,26 @@ public class Darkness : MonoBehaviour
     void Update()
     {
         Vector3 scale = gameObject.GetComponent<Transform>().localScale;
-        scale += new Vector3(0, 0.005F, 0);
+        scale += new Vector3(scaleFactor, scaleFactor, 0);
         gameObject.GetComponent<Transform>().localScale = scale;  
     
+    }
+
+    public void AddMultiplier(float add)
+    {
+        multiplier += add;
     }
 
     void OnTriggerEnter2D(Collider2D coll) {
         Debug.Log("entered");
         if(coll.tag == "Player") {
-            coll.gameObject.GetComponent<PlayerStatus>().SetMaxHealth(
-                coll.gameObject.GetComponent<PlayerStatus>().GetMaxHealth()-1f);
+            coll.gameObject.GetComponent<PlayerStatus>().TakeDamage(.1f*multiplier);
         }
     }
  
     void OnTriggerStay2D(Collider2D coll){
         if(coll.tag == "Player") {
-            coll.gameObject.GetComponent<PlayerStatus>().SetMaxHealth(
-                coll.gameObject.GetComponent<PlayerStatus>().GetMaxHealth()-1f);
+            coll.gameObject.GetComponent<PlayerStatus>().TakeDamage(.1f*multiplier);
         }
     } 
 
--- Assets/Scripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerStatus.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class PlayerStatus : MonoBehaviour
 {
+    private GameObject[] lightLevels;
+    private int lightLevelIndex = 1;
     private float upgradeHealthValue = 100f;
 
     private float maxHealth = 100f;
     private float curHealth = 100f;
 
     private DungeonJuiceScript djs;
+
+    private Darkness ds;
     private HealthBarScript hbs;
     
     // Start is called before the first frame update
@@ -26,6 +30,41 @@ public class PlayerStatus : MonoBehaviour
         {
             Debug.Log("Error: PlayerStatus could not find object \"HealthBarScript\".");
         }
+
+        ds = GameObject.Find("Darkness").GetComponent<Darkness>();
+        if (ds == null)
+        {
+            Debug.Log("Error: Darkness (script) could not find object \"Darkness\".");
+        }
+
+        lightLevels = new GameObject[4];
+
+        lightLevels[0] = GameObject.Find("circle1");
+        if (lightLevels[0] == null)
+        {
+            Debug.Log("Error: PlayerStatus could not find object \"circle1\".");
+        }
+        lightLevels[1] = GameObject.Find("circle2");
+        if (lightLevels[1] == null)
+        {
+            Debug.Log("Error: PlayerStatus could not find object \"circle2\".");
+        }
+        lightLevels[2] = GameObject.Find("circle3");
+        if (lightLevels[2] == null)
+        {
+            Debug.Log("Error: PlayerStatus could not find object \"circle3\".");
+        }
+        lightLevels[3] = GameObject.Find("circle4");
+        if (lightLevels[3] == null)
+        {
+            Debug.Log("Error: PlayerStatus could not find object \"circle4\".");
+        }
+
+        lightLevels[0].SetActive(false);
+        lightLevels[1].SetActive(false);
+        lightLevels[2].SetActive(false);
+        lightLevels[3].SetActive(false);
+
     }
 
     // Update is called once per frame
@@ -41,11 +80,13 @@ public class PlayerStatus : MonoBehaviour
 
     private void Upgrade()
     {
-        if (djs.GetDungeonJuice() >= 50f) 
+        if (djs.GetDungeonJuice() >= 50f && lightLevelIndex < 4) 
         {
             djs.AddToDungeonJuice(-50f);
-            maxHealth += upgradeHealthValue;
-            curHealth += upgradeHealthValue;
+            ds.AddMultiplier(-.3f);
+
+            lightLevels[lightLevelIndex].SetActive(true);
+            lightLevelIndex++;
         }
     }
 
